95a3c3160ab6c369357248fa380cd89130854a90
Martin Vainer <114117778+martin-vainer@users.noreply.github.com>
2024-04-03 12:47:48 -0300

    probando IDE - agrego junit

---
+++ src/test/java/domain/InscripciónTest.java
@@ -0,0 +1,11 @@
+package domain;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class InscripciónTest {
+@Test
+    public void testPrueba(){
+        Assert.assertEquals();
+    }
+}
